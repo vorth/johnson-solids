009b223394b6213a05576f0d641962995b097aaa
David Hall <David.42.Hall@gmail.com>
2023-12-10 21:39:14 -0500

    Utilize edgescene and facescene from the JSON
    Had to change edgeScene and faceScene in the JSON from camelCase to lowercase edgescene and facescene to comply with html data-* naming requirements which don't allow any uppercase letters.

--- johnson-solids-listing.js
+++ johnson-solids-listing.js
@@ -1,16 +1,16 @@
 import { models } from './johnson-solids-models.js';
 
 let selectedRow;
-let scenes;
-
 const table = document.getElementById( "partsTable" );
 const tbody = table.createTBody();
 const viewer = document.getElementById( "viewer" );
 const showEdges = document.getElementById( "showEdges" );
+const zomeSwitch = document.getElementById( "zome-switch" );
 
 viewer .addEventListener( "vzome-scenes-discovered", (e) => {
-  scenes = e.detail;
-  console.log( "scenes discovered in " + viewer.src + "\n" + JSON.stringify( scenes, null, 2 ) );
+  // Just logging this to the console for now. Not actually using the scenes list.
+  const scenes = e.detail;
+  console.log( "These scenes were discovered in " + viewer.src + "\n" + JSON.stringify( scenes, null, 2 ) );
 } );
 
 for (const jsolid of models) {
@@ -24,30 +24,32 @@ for (const jsolid of models) {
 selectJohnsonSolid( models[ 0 ], tbody .rows[ 0 ] );
 showEdges.addEventListener("change", // use "change" here, not "click"
   () => {
-    const { field, edgeScene, faceScene, zometool } = selectedRow.dataset;
-    setScene(field, zometool);
+    setScene(selectedRow.dataset);
   } );
 
 function selectJohnsonSolid( jsolid, tr ) {
-  const { url, id } = jsolid;
-    if(url) {
-      if ( selectedRow ) {
-        selectedRow.className = "";
-      }
-      selectedRow = tr;
-      selectedRow.className = "selected";
-      document.getElementById( "index" ).textContent = "J" +id;
-      switchModel(jsolid);
-  } else {
-      alert("Johnson solid J" + id + " is not yet available.\n\nPlease help us collect the full set.");
-  }
+	if(tr != selectedRow) {
+	  const { url, id } = jsolid;
+		if(url) {
+		  if ( selectedRow ) {
+			selectedRow.className = "";
+		  }
+		  selectedRow = tr;
+		  selectedRow.className = "selected";
+		  document.getElementById( "index" ).textContent = "J" +id;
+		  switchModel(jsolid);
+	  } else {
+		  alert("Johnson solid J" + id + " is not yet available.\n\nPlease help us collect the full set.");
+	  }
+	}
 }
 
 function fillRow(tr, jsolid) {
-  const { id, title, field, url, edgeScene, faceScene, zometool } = jsolid;
+  const { id, title, field, url, edgescene, facescene, zometool } = jsolid;
+  // Data attribute names must be prefixed with 'data-' and should not contain any uppercase letters,
   tr.setAttribute("data-field", field);
-  tr.setAttribute("data-edgeScene", edgeScene);
-  tr.setAttribute("data-faceScene", faceScene);
+  tr.setAttribute("data-edgescene", edgescene);
+  tr.setAttribute("data-facescene", facescene);
   tr.setAttribute("data-zometool", !!zometool);
   if(!tr.id) {
     tr.id = "jsolid-" + id;
@@ -68,27 +70,18 @@ function fillRow(tr, jsolid) {
 }
 
 function switchModel( jsolid ) {
-  const { field, url, zometool } = jsolid;
-  viewer.src = url;
-  setScene( field, zometool );
+  viewer.src = jsolid.url;
+  setScene( jsolid );
 }
 
-function setScene( field, zometool ) {
-  const scene = sceneFor(field, zometool);
-  document.getElementById( "zome-switch" ).className = ( !!scene && !!zometool ) ? 'zome' : 'no-zome';
+function setScene( jsolidSceneData ) {
+  // jsolidSceneData may be a jsolid object from the JSON
+  /// or it may be selectedRow.dataset.
+  // Either one should have these properties, all in lower case
+  const { field, edgescene, facescene, zometool } = jsolidSceneData;
+  const scene = field == "Golden" && zometool == "true" && showEdges.checked ? edgescene : facescene;
+  zomeSwitch.className = ( !!zometool ) ? 'zome' : 'no-zome';
   viewer.scene = scene;
+  console.log("Setting scene to '" + scene + "'");
   viewer.update({ camera: false });
-  console.log("setScene( '" + field + "', '" + zometool + "' ) = '" + scene + "'");
 }
-
-function sceneFor( field, zometool ) {
-  // TODO: update this to use edgeScene and faceScene from the json
-  // instead of relying on naming convention.
-  // That will also allow multiple solids to share the same vZome file.
-  return field == "Golden" && zometool == "true"
-    ? showEdges.checked 
-      ? "Edges"
-      : "Faces"
-    : "";
-}
-
