dccdd3b3a511c86583d1a5584f3dbb2b985459e3
David Hall <David.42.Hall@gmail.com>
2024-02-01 17:45:31 -0500

    Replace J3 with one using the standard zometool ball

--- johnson-solids-models.js
+++ johnson-solids-models.js
@@ -1,7 +1,7 @@
 export const models = [
   { id:1 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Square pyramid", url:"uploads/david-hall/J1-Square-Pyramid-Golden.vZome"},
   { id:2 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Pentagonal pyramid", url:"uploads/vorth/J2-Pentagonal-Pyramid-Golden.vZome"},
-  { id:3 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Triangular cupola", url:"uploads/Jackpike/J3-Triangular-cupola-Golden.vZome"},
+  { id:3 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Triangular cupola", url:"uploads/david-hall/J3-Triangular-cupola-Golden.vZome"},
   { id:4 , field:"Root2", edgescene:"J4 Edges", facescene:"J4 Faces", title:"Square cupola", url:"uploads/david-hall/J4-and-J19-Root2.vZome"},
   { id:5 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Pentagonal cupola", url:"uploads/david-hall/J5-Pentagonal-cupola-Golden.vZome" },
   { id:6 , field:"Golden", edgescene:"Edges", facescene:"Faces", zometool:"true", title:"Pentagonal rotunda", url:"uploads/Jackpike/J6-Pentagonal-rotunda-Golden.vZome" },
